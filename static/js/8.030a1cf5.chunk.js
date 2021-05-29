c416684fee921929eb0a952652eb286cadbe53a3
AnastasiiaOle <anastasiiaole@gmail.com>
2021-05-29 20:01:24 +0200

    Updates

---
+++ static/js/8.030a1cf5.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-04-movies"]=this["webpackJsonpgoit-react-hw-04-movies"]||[]).push([[8],{74:function(t,e,o){"use strict";o.r(e);o(0);var i=o(1);e.default=function(){return Object(i.jsx)("h1",{children:" Sorry, page not found. "})}}}]);
+//# sourceMappingURL=8.030a1cf5.chunk.js.map
